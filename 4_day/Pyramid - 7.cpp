0d0aa358cbc9e24782ce7ab17411596a098fbf63
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-18 21:28:10 +0530

    Update Pyramid - 7.cpp

--- 4_day/Pyramid - 7.cpp
+++ 4_day/Pyramid - 7.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
-#include<cstdio>
-#include<cmath>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
 using namespace std;
 int main()
 {
-    int n,num=1;
-    cin>>n;
-    for(int i=1; i<=n; i++)
-    {   
-        for(int j=1; j<=i; j++)
+    int n, num = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
         {
-            cout<<num;
-            if (j < i) { 
+            cout << num;
+            if (j < i)
+            {
                 cout << " ";
             }
             num++;
         }
-        cout<<endl;
+        cout << endl;
     }
-    
-   return 0;
+
+    return 0;
 }
