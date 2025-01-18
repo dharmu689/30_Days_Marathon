329083e9fca8fccbc5440579aa97170dafcb5907
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-18 21:27:35 +0530

    Create Pyramid - 7.cpp

---
+++ 4_day/Pyramid - 7.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,num=1;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {   
+        for(int j=1; j<=i; j++)
+        {
+            cout<<num;
+            if (j < i) { 
+                cout << " ";
+            }
+            num++;
+        }
+        cout<<endl;
+    }
+    
+   return 0;
+}
