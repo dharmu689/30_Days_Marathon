f4313923c63e9c698654a5a185f0e5d841d1ec38
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-22 12:02:38 +0530

    Create Dynamic allocate an array and then find out the average of the elements.cpp

---
+++ 7_day/Dynamic allocate an array and then find out the average of the elements.cpp
@@ -0,0 +1,15 @@
+/*int *dynamicMemoryAllocateArray(int n){
+     int* arr = (int*)malloc(n * sizeof(int));
+    if (!arr) {
+        exit(1);
+    }
+    return arr;
+}
+float findAverage(int *arr, int n){
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return static_cast<float>(sum) / n;
+}
+*/
