76aef4d4c37634430029fb462b7d1eea3ce56f5a
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-29 10:35:23 +0530

    Create Reverse the words of a string.cpp

---
+++ 14_day/Reverse the words of a string.cpp
@@ -0,0 +1,21 @@
+
+
+
+/*#include<bits/stdc++.h>
+void revWordsString(string str) {
+  stringstream ss(str);
+    string word, result;
+
+    while (ss >> word) {
+        // Reverse the characters within the word
+        reverse(word.begin(), word.end()); 
+        result += word + " "; 
+    }
+
+    // Remove the trailing space
+    if (!result.empty()) {
+        result.pop_back();
+    }
+
+    cout<<result;
+}*/
