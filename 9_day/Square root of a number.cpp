f744c8622d18dcff96273b499d88df81eb8a5805
Dharmu kumar <dharmukumar1622003@gmail.com>
2025-01-29 10:35:14 +0530

    Create Square root of a number.cpp

---
+++ 9_day/Square root of a number.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cmath>
+// Include headers as needed
+
+using namespace std;
+
+int main()
+{
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << fixed << setprecision(2) << sqrt(n) << endl;
+    }
+
+    // Return 0 to indicate normal termination
+    return 0;
+}
